dd90ea23ae3109780cb7d9e6445f0abeec1a5ea3
CsPaulo <cspaulorafael@gmail.com>
2025-02-10 11:50:50 -0300

    aprimora o código main

--- alura-midi/main.js
+++ alura-midi/main.js
@@ -1,11 +1,28 @@
 function tocaSom(idAudio) {
-  document.querySelector(id).play();
+  const elemento = document.querySelector(idAudio).play();
+  if (elemento && elemento.localName === 'audio') {
+    elemento.play();
+  } else {
+    console.log('Elemento não encontrado ou seletor inválido');
+  }
 }
-
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-while (contador < listaDeTeclas.length) {
-  listaDeTeclas[contador].onlick = tocaSom;
-  contador++;
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+
+  tecla.onclick = function () {
+    tocaSom(`#som_${instrumento}`);
+  };
+
+  tecla.onkeydown = function (evento) {
+    if (evento.code === 'Space' || evento.code === 'Enter') {
+      tecla.classList.add('ativa');
+    }
+  };
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
+  };
 }

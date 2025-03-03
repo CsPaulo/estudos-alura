e2a0e1cb63778f87333caecc3e87f88785ad4cdb
CsPaulo <cspaulorafael@gmail.com>
2025-03-03 14:25:14 -0300

    feat: add controllers

--- express-mongo/src/app.js
+++ express-mongo/src/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import conectaNaDatabase from "./config/dbConnect.js";
-import livro from "./models/Livro.js";
-
+import routes from "./routes/index.js";
 const conexao = await conectaNaDatabase();
 
 conexao.on("error", (erro) => {
@@ -12,36 +11,6 @@ conexao.once("open", () => {
   console.log("Conectado no banco de dados");
 });
 const app = express();
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.status(200).send("Bem vindo ao servidor");
-});
-
-app.get("/livros", async (req, res) => {
-  const listaLivros = await livro.find({});
-  res.status(200).json(listaLivros);
-});
-
-app.get("/livros/:id", (req, res) => {
-  const index = buscaLivro(req.params.id);
-  res.status(200).json(livros[index]);
-});
-app.post("/livros", (req, res) => {
-  livros.push(req.body);
-  res.status(201).send("Livro adicionado com sucesso");
-});
-
-app.put("/livros/:id", (req, res) => {
-  const index = buscaLivro(req.params.id);
-  livros[index].nome = req.body.nome;
-  res.status(200).send("Livro atualizado com sucesso");
-});
-
-app.delete("/livros/:id", (req, res) => {
-  const index = buscaLivro(req.params.id);
-  livros.splice(index, 1);
-  res.status(200).send("Livro deletado com sucesso");
-});
+routes(app);
 
 export default app;
